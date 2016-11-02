0e1f9beb65c165e33807ea1fc70a0c74468f6cdf
Matt Smith <msmith@apaxsoftware.com>
2016-11-02 13:40:30 -0400

    Initial testing.

--- jquery-highlight-selection-test.js
+++ jquery-highlight-selection-test.js
@@ -1,6 +1,22 @@
 const test = require('tape');
 
+// Setup a browser environment
+require('browser-env')();
+
+// Setup jQuery and the plugin
+const jQuery = require('jquery');
+const $ = jQuery;
+require('./jquery-highlight-selection')();
+
+
 test('highlightSelection is a function on jQuery collection', (t) => {
     t.plan(1);
-    t.equal(1, 1);
+    t.equal(typeof $('div').highlightSelection, 'function');
+});
+
+test('returns the same jQuery collection plugin was invoked on',  (t) => {
+    t.plan(1);
+    divs = $('div');
+
+    t.deepEqual(divs.highlightSelection(), divs);
 });
